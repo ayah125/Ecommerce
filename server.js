602cd5d08ec56a431f95236f3a0086dfe0551f6a
ayah125 <ayahalyy7@gmail.com>
2024-02-17 23:40:41 +0200

    first commit

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 const port = 3000;
+// echo "# ECCO" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/ayah125/ECCO.git
+// git push -u origin main
 app.use(express.json());
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
